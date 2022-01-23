2d921e80dffd14819df60a759260e9f2993a17d2
Claudio <cernandes.silva@gmail.com>
2022-01-23 16:32:47 -0300

    UserDTO

---
+++ src/main/java/com/example/trabalhobootcampspring/dto/UserDTO.java
@@ -0,0 +1,113 @@
+package com.example.trabalhobootcampspring.dto;
+
+import java.time.Instant;
+
+import com.example.trabalhobootcampspring.entities.User;
+
+public class UserDTO {
+
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+
+	public UserDTO() {
+
+	}
+
+	public UserDTO(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+
+	public UserDTO(User user) {
+
+		id = user.getId();
+		name = user.getName();
+		cpf = user.getCpf();
+		income = user.getIncome();
+		birthDate = user.getBirthDate();
+		children = user.getChildren();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserDTO other = (UserDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
